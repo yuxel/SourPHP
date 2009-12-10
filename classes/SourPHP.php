11aa1c15e6400af0a663757d5bdcf9eec957732d
yuxel <yuxel@sonsuzdongu.com>
2009-12-11 00:26:01 +0200

    fetching of id and entry finished, can now get number of pages

--- classes/SourPHP.php
+++ classes/SourPHP.php
@@ -23,14 +23,199 @@
 class sourPHP{
 
     /**
+     * Default request address
+     *
+     * @var String 
+     */
+    protected static $url = null;
+
+    /**
+     * Init default parameters
+     */
+    public function __construct() {
+        $this->url = "http://sozluk.sourtimes.org/";
+    }
+
+
+
+    /**
      * fetchs data from Url
      *
      * @param  string $url
-     * @return string Content of url
+     * @return string Content of url, returns false if fails
      */
     function fetchUrl($url) {
-        $data = file_get_contents($url);
-        return $data;
+        if( $data = file_get_contents($url) ) {
+            return $data;
+        }
+        return false;
     }
- 
+
+    /**
+     * fetchs an entry id's data
+     *
+     * @param  int $entryId
+     * @return string Content of url
+     */
+    function fetchId( $entryId ) {
+        $entryId = (int) $entryId;
+        if($entryId < 1) {
+            /**
+             * if entry id is not valid
+             */
+            return false;
+        }
+        $url = $this->url."show.asp?id=".$entryId;
+
+        return $this->fetchUrl($url);
+    }
+
+    /**
+     * fetchs an entry per page
+     *
+     * @param  string $entryName
+     * @param  int $page
+     * @return string Content of entry
+     */
+    function fetchEntry( $entryName , $page=1) {
+        $page = (int) $page;
+        $entryName = (string) $entryName;
+        if(strlen($entryName) < 1 ) {
+            /**
+             * if entry name is not valid
+             */
+            return false;
+        }
+        $encodedEntryName = urlencode( trim($entryName) );
+
+        $pageToGo = 1;
+        if($page > 1 ) {
+            $pageToGo = $page;
+        }
+
+        $url = $this->url."show.asp?t=".$encodedEntryName."&p=".$pageToGo;
+
+        return $this->fetchUrl($url);
+    }
+
+
+
+   /**
+     * creates new DomDocument from data retreived from url
+     *
+     * @param string $data
+     * @return DomDucument object
+     */
+    function createDomDocumentFromData($data) {
+        $doc = new DOMDocument();
+        $doc->loadHTML ($data);
+        return $doc;
+    }
+
+
+    /**
+     * executes and XPathQuery to Document and returns results
+     * as array
+     *
+     * @param DomDocument $doc
+     * @param string XPathQuery like "//div"
+     * @return array $results
+     */
+    function XPathQueryToDoc($doc, $queryString){
+        $xpath = new DOMXpath($doc);
+        $elements = $xpath->query($queryString);
+        
+        return $elements; 
+
+    }
+
+    /**
+     * finds entry title from read document
+     *
+     * @param DomDucument $doc
+     * @return string title
+     */
+    function getEntryTitleFromDoc($doc) {
+       
+        $query = "//html/body/h1[@class='title']";
+        $nodeList = $this->XPathQueryToDoc($doc, $query);
+
+        foreach($nodeList as $node) {
+            return trim($node->nodeValue);
+        }
+        
+        return false;
+    }
+
+
+    /**
+     * returns number of total pages from read document
+     *
+     * @param DomDucument $doc
+     * @return int number of pages
+     */
+    function getNumberOfPages($doc) {
+
+        $query = "//select[@class='pagis']/option";
+        $nodeList = $this->XPathQueryToDoc($doc, $query);
+
+        foreach($nodeList as $node) {
+            $totalPageNum = $node->nodeValue;
+        }
+
+        return (int) $totalPageNum;
+    }
+
+    /**
+     * returns content of entry from read document
+     *
+     * @param DomDucument $doc
+     * @return array entries[content, author, id, order, datetime]
+     */
+    function getContentOfEntriesFromDoc($doc) {
+        $xpath = new DOMXPath($doc);
+   
+        $query = "//ol[@id='el']/li";
+        $metaDivQuery = "//div[@class='aul']";
+        $authorQuery = "//div[@class='aul']/a/text()";
+        $dateTimeQuery = "//div[@class='aul']/text()";
+
+        $nodeList = $this->XPathQueryToDoc($doc, $query);
+        
+        foreach($nodeList as $node) {
+
+            $entryId = $node->getAttribute('id');
+            $entryId = preg_replace ( '/[^0-9]/', '', $entryId );
+
+            $order = (int) $node->getAttribute("value");
+
+            $authorNodes = $xpath->evaluate ($authorQuery, $node);
+            foreach($authorNodes as $authorNode) {
+                $author = $authorNode->nodeValue;
+            }
+           
+            $dateTimeNodes = $xpath->evaluate ($dateTimeQuery, $node);
+            foreach($dateTimeNodes as $dateTimeNode) {
+                $dateTime = explode( "~", trim($dateTimeNode->nodeValue,")") );
+            
+                $dateCreated = trim(trim($dateTime[0],","));
+                $dateEdited = trim($dateTime[1]);
+
+            }
+
+            $results[] = array("entryId"=>$entryId,
+                               "order"=>$order,
+                               "author"=>$author,
+                               "dateCreated"=>$dateCreated,
+                               "dateEdited"=>$dateEdited);
+
+            var_dump($results);
+            return true;
+        }
+
+
+
+
+
+    }
 }
--- tests/SourPHPTest.php
+++ tests/SourPHPTest.php
@@ -32,16 +32,132 @@ class SourPHPTest extends PHPUnit_Framework_TestCase
     }
 
 
-    /**
-     * Test for fetching url
-     *
-     * @test
-     */
-    function ifDataReturnedOnFetch() {
+    function testIfDataReturnedOnFetch() {
         $result = $this->obj->fetchUrl("http://sozluk.sourtimes.org/show.asp?t=madde+97%2F%2317410156&pad=1");
         $this->assertNotNull ($result);
     }
 
+
+    function testFetchingIntegerId() {
+        $result = $this->obj->fetchId(17410156);
+        $this->assertNotNull ($result);
+    }
+
+    /**
+     * test if paramter is a string but can be casted as integer
+     */
+    function testFetchingStringButCanBeCastToAnIntegerId() {
+        $result = $this->obj->fetchId("17410156");
+        $this->assertNotNull ($result);
+    }
+
+    function testFetchingNonInteger() {
+        $result = $this->obj->fetchId("this_is_not_valid");
+        $this->assertFalse ($result);
+    }
+
+    function testFetchEntry() {
+        $result = $this->obj->fetchEntry("php");
+        $this->assertNotNull ($result);
+    }
+
+    function testFetchEntryWhichHasTurkishChars() {
+        $result = $this->obj->fetchEntry("neşet ertaş");
+        $this->assertNotNull ($result);
+    }
+
+
+
+
+    /**
+     * test if document created successfully
+     */
+    function testToCreateDomDocumentFromData() {
+        $data = $this->obj->fetchId(17410156);
+        $returned = $this->obj->createDomDocumentFromData($data);
+
+        $result =($returned instanceof DomDocument)?true:false;
+        $this->assertTrue($result);
+    }
+
+
+    function testXpathQuery(){
+        $contentOfFirstDiv="hello";
+        $data = "<div id=\"test\">$contentOfFirstDiv</div><div>Second content</div>";
+        $doc = $this->obj->createDomDocumentFromData($data);
+
+        $returned = $this->obj->XPathQueryToDoc($doc, "*//div");
+
+        $result =($returned instanceof DomNodeList)?true:false;
+
+        $this->assertTrue($result);
+    }
+
+
+    /**
+     * this should return "php" as title
+     */
+    function testGetEntryTitleFromDoc() {
+        $entryId = 8102; //entry for "php" 
+        $data = $this->obj->fetchId(8102);
+        $doc = $this->obj->createDomDocumentFromData($data);
+
+        $title = $this->obj->getEntryTitleFromDoc($doc);
+
+        $this->assertEquals($title, "php");
+
+    }
+
+
+    /**
+     * "php" entry has 7 pages for now
+     * @fixme this could change later
+     */
+    function testGetNumberOfPages(){
+        $data = $this->obj->fetchEntry("php");
+        $doc = $this->obj->createDomDocumentFromData($data);
+
+        $numOfPages = $this->obj->getNumberOfPages($doc);
+
+        $this->assertEquals($numOfPages, 7);
         
+    }
+
+    /**
+     * there's no such php_thats_not_exists entry so page num should be 0
+     */
+    function testGetNumberOfPagesForAnEntryWhichNotExists(){
+        $data = $this->obj->fetchEntry("php_thats_not_exist");
+        $doc = $this->obj->createDomDocumentFromData($data);
+
+        $numOfPages = $this->obj->getNumberOfPages($doc);
+
+        $this->assertEquals($numOfPages, 0);
+        
+    }
+
+    /**
+     * 'neşet ertaş' page (turkish chars and white space tested)
+     */
+    function testGetNumberOfPagesForAnEntryWhichContainsTurkishCharsAndWhitespace(){
+        $data = $this->obj->fetchEntry("neşet ertaş");
+        $doc = $this->obj->createDomDocumentFromData($data);
+
+        $numOfPages = $this->obj->getNumberOfPages($doc);
+
+        $this->assertEquals($numOfPages, 10);
+        
+    }
+
+
+    function testGetContentsOfEntriesFromDoc() {
+        $data = $this->obj->fetchEntry("neşet ertaş");
+        $doc = $this->obj->createDomDocumentFromData($data);
+
+        $entries = $this->obj->getContentOfEntriesFromDoc($doc);
+
+
+    }
+
 }
 
