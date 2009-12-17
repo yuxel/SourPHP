1e5c54930cf1c56747b3b6bce28a12aab966efe1
yuxel <yuxel@sonsuzdongu.com>
2009-12-17 22:53:48 +0200

    Main class interface

---
+++ classes/SourPHPInterface.php
@@ -0,0 +1,30 @@
+<?
+/**
+ * SourPHPInterface
+ *
+ * LICENSE
+ *
+ * This file can be distributed under terms of LGPL
+ *
+ * @package    SourPHP
+ * @copyright  Osman Yuksel
+ * @LICENSE    http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @version    0.1
+ */
+
+
+
+/**
+ * SourPHPInterface
+ *
+ * @author    Osman Yuksel <yuxel |AT| sonsuzdongu |DOT| com>
+ */
+interface SourPHPInterface{
+
+    //@todo comments
+    public function getEntryById($entryId);
+
+    
+    public function getEntryByTitle($entryTitle); 
+
+}
